48199dd15c423b978a586d44b5b41abeb1f4d141
KS-HTK <2981026+KS-HTK@users.noreply.github.com>
2025-12-09 08:27:32 +0100

    updated day09

--- day09/day09.py
+++ day09/day09.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
-from collections import defaultdict
 from time import perf_counter
+from itertools import combinations
+from shapely import Polygon, box
 
 
 def profiler(method):
@@ -14,56 +15,15 @@ def profiler(method):
     return profiler_method
 
 
-# Part 1:
-def part1(content=None) -> int:
-    max_floor = 0
-    for a1, b1 in content:
-        for a2, b2 in content:
-            floor = (max(a1, a2)+1-min(a1, a2)) * (max(b1, b2)+1-min(b1, b2))
-            max_floor = max(floor, max_floor)
-    return max_floor
-
-
-def get_rect_coords(p1: tuple[int, int], p2: tuple[int, int]) -> set[tuple[int, int]]:
-    out = set()
-    for a in range(min(p1[0], p2[0]), max(p1[0], p2[0])+1):
-        for b in range(min(p1[1], p2[1]), max(p1[1], p2[1])+1):
-            out.add((a, b))
-    return out
-            
-
-def valid_rectangle(p1, p2, green):
-    min_x, max_x = min(p1[0], p2[0]), max(p1[0], p2[0])
-    min_y, max_y = min(p1[1], p2[1]), max(p1[1], p2[1])
-    
-    for y in range(min_y, max_y+1):
-        if y not in green:
-            return False
-        bound_min_x, bound_max_x = green[y]
-        if min_x < bound_min_x or bound_max_x < max_x:
-            return False
-    return True
-
-
-# Part 2:
-def part2(content=None) -> int:  # 1550760868
-    outline = set()
-    last = content[-1]
-    for p in content:
-        outline |= get_rect_coords(last, p)
-        last = p
-    bounds = defaultdict(list)
-    for x, y in outline:
-        bounds[y].append(x)
-    for y in bounds:
-        bounds[y] = [min(bounds[y]), max(bounds[y])]
-    
-    max_area = 0
-    for i, p1 in enumerate(content):
-        for p2 in content[i+1:]:
-            if valid_rectangle(p1, p2, bounds) and (m := (1 + abs(p1[0] - p2[0])) * (1 + abs(p1[1] - p2[1]))) > max_area:
-                max_area = m
-    return max_area
+def parts(red_tiles) -> tuple[int, int]:
+    outer = Polygon(red_tiles)
+    pt1, pt2 = 0, 0
+    for (x1, y1), (x2, y2) in combinations(red_tiles, 2):
+        area = (1 + abs(x1 - x2)) * (1 + abs(y1 - y2))
+        pt1 = max(pt1, area)
+        if outer.contains(box(min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2))):
+            pt2 = max(pt2, area)
+    return pt1, pt2
 
 
 def get_input():
@@ -74,9 +34,9 @@ def get_input():
 
 @profiler
 def solve():
-    content = get_input()
-    print(f'Part 1: {part1(content)}')
-    print(f'Part 2: {part2(content)}')
+    part1, part2 = parts(get_input())
+    print(f'Part 1: {part1}')
+    print(f'Part 2: {part2}')
 
 
 if __name__ == "__main__":
